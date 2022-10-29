b016f3aac7d01222e290ff0145f80f550195899a
Vandorc-Dev <vandodev@gmail.com>
2022-10-29 13:03:33 -0300

    Finalizado os componentes - Versão 1.o do bloh pronta

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,24 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "Qualquer coisa!",
+        "title": "Saiu nova versão do angular",
+        "description": "Este é ´so um exempl de descrição mesmo...",
+        "photoCover": "https://victorroblesweb.es/wp-content/uploads/2017/04/angular4.png7/04/angular4.png"
+    },
+    {
+        "id": "2",
+        "photo": "Noticia 2",
+        "title": "Saiu nova versão do angular",
+        "description": "Este é ´so um exempl de descrição mesmo...",
+        "photoCover": "https://poltronanerd.com.br/wp-content/uploads/2022/10/titans-1665381692361.png"
+    },
+    {
+        "id": "3",
+        "photo": "Noticia 3 - Segue o fluxo...",
+        "title": "Saiu nova versão do angular",
+        "description": "Este é ´so um exempl de descrição mesmo...",
+        "photoCover": "https://poltronanerd.com.br/wp-content/uploads/2022/10/titans-1665381692361.png"
+    },
+    
+]
